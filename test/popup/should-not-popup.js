af94d01bcfb197488f091a1f49c39438c2343b2a
unknown <alphonnsoallarde@gmail.com>
2017-12-04 10:29:56 +0800

    Add test specs for Piggy pop-up :beer:

---
+++ test/popup/should-not-popup.js
@@ -0,0 +1,25 @@
+var webdriverio = require('webdriverio');
+var assert = require('assert');
+var chai = require('chai');
+var chaiWebdriver = require('chai-webdriverio').default;
+
+var fs = require('fs');
+
+chai.use(chaiWebdriver(browser));
+
+describe('Piggy auto-pop test run', function() {
+  beforeEach(function() {
+    browser.url('https://www.samsclub.com/sams/cart/cart.jsp?eventId=scCheckout&sortProperties=-lastModifiedDate');
+  });
+
+  it('Piggy should not pop-up', function() {
+    var title = /^https:\/\/www\.([\w]+)\.com.*/.exec(browser.getUrl())[1];
+    var screenshotPath = './screenShots/';
+    var filePath = screenshotPath + title + '-not.png';
+
+    expect('#piggyPromptTryCodes').to.not.be.there();
+
+    browser.saveScreenshot(filePath);
+    console.log('Screenshot: ' + filePath + ', saved!');
+  });
+});
